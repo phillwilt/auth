a16b9a7f8dfb8b812bcee6462ab83691405febc2
Phillip Wilt <phillip.wilt@gmail.com>
2014-09-24 20:06:36 -0700

    auth poc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '9eab2177157b2ea09595d198be9ed669cf171f2bc5657a8129999b701bd28ca9d814f47c6d1388b7dd82f46353d10c8782a91b38b52f8a688001cef8356c0d6f'
